428a3e0e3d34d7927e6eacf7ee935bff9aaec5d5
AshwiniRatod <ashuratod40@gmail.com>
2024-11-17 20:26:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let imagebtn = document.querySelector("#image");
 let image = document.querySelector("#image img");
 let imageinput = document.querySelector("#image input");
 
-const Api_Url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyC95_C7fbNECo04Udu0OSZZg5YukbvxySs"
+const Api_Url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=your api"
 
 
 let user = {
